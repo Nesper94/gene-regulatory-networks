e87374cb40af059c6560c85398ddc6e9580e0544
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-05-03 10:15:13 -0500

    cambiar rutas absolutas por relativas
    En específico se cambiaron las funciones 'importGRNs' y 'importProcrustedGRNs', aunque la idea es cambiarlas todas.

--- modules/importData.m
+++ modules/importData.m
@@ -1,17 +1,26 @@
 (*Paquete para importar datos.*)
 
-importGRNs::usage="importGRNs[  ] importa la lista de GRNs clasificadas no isomórficas -> {GRN, número de topología}."
+importGRNs::usage="importGRNs[  ] importa la lista de GRNs clasificadas no
+isomórficas -> {GRN, número de topología}."
 
-importGRNs[]:= Flatten[ToExpression[#],1]&/@Import["/home/nesper94/Documentos/biologia-de-sistemas/RobustnessModularityProject/RobustnessModularityProject/motivos_clasificados_ordenados_no_isomorfos.txt","TSV"]
+Print[FileNameJoin[
+{ParentDirectory[NotebookDirectory[]],"data","motivos-clasificados-ordenados-no-isomorfos.txt"}]]
 
-importTopologies::usage="importTopologies[  ] importa la lista de topologías ordenadas según su respectiva abundancia -> {topología, ranking de abundancia}."
+importGRNs[]:= Flatten[ToExpression[#],1]&/@Import[ FileNameJoin[
+{ParentDirectory[NotebookDirectory[]],"data","motivos-clasificados-ordenados-no-isomorfos.txt"}],"TSV"]
 
-importTopologies[]:= Flatten[ToExpression[#],1]&/@Import["/home/nesper94/Documentos/biologia-de-sistemas/RobustnessModularityProject/RobustnessModularityProject/lista_de_topologías.txt","TSV"]
+importTopologies::usage="importTopologies[  ] importa la lista de topologías
+ordenadas según su respectiva abundancia -> {topología, ranking de abundancia}."
 
-importAbundanceTopol::usage="importAbundanceTopol[  ] importa la lista de topologías y su abundancia -> {topología, abundancia}."
+importTopologies[]:= Flatten[ToExpression[#],1]&/@Import[
+"/home/nesper94/Documentos/biologia-de-sistemas/RobustnessModularityProject/RobustnessModularityProject/lista_de_topologías.txt","TSV"]
+
+importAbundanceTopol::usage="importAbundanceTopol[  ] importa la lista de
+topologías y su abundancia -> {topología, abundancia}."
 
 importAbundanceTopol[]:= Flatten[ToExpression[#],1]&/@Import["/home/nesper94/Documentos/biologia-de-sistemas/RobustnessModularityProject/RobustnessModularityProject/motivos_de_red.txt","TSV"]
 
 importProcrustedGRNs::usage="importProcrustedGRNs[  ]"
 
-importProcrustedGRNs[]:=Flatten[ToExpression[#],1]&/@Import["/home/nesper94/Documentos/biologia-de-sistemas/RobustnessModularityProject/RobustnessModularityProject/procrusted_GRNs.txt","TSV"]
+importProcrustedGRNs[]:=Flatten[ToExpression[#],1]&/@Import[ FileNameJoin[
+{ParentDirectory[NotebookDirectory[]],"data","procrusted-grns.txt"}],"TSV"]
