360f2c7280dae916283ea54139795b12aab11f7a
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-05-04 15:34:24 -0500

    implementar importe relativo de datos

--- modules/importData.m
+++ modules/importData.m
@@ -3,21 +3,22 @@
 importGRNs::usage="importGRNs[  ] importa la lista de GRNs clasificadas no
 isomórficas -> {GRN, número de topología}."
 
-importGRNs[]:= Flatten[ToExpression[#],1]&/@Import[ FileNameJoin[
+importGRNs[]:= Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
 {ParentDirectory[NotebookDirectory[]],"data","motivos-clasificados-ordenados-no-isomorfos.txt"}],"TSV"]
 
 importTopologies::usage="importTopologies[  ] importa la lista de topologías
 ordenadas según su respectiva abundancia -> {topología, ranking de abundancia}."
 
-importTopologies[]:= Flatten[ToExpression[#],1]&/@Import[
-"/home/nesper94/Documentos/biologia-de-sistemas/RobustnessModularityProject/RobustnessModularityProject/lista_de_topologías.txt","TSV"]
+importTopologies[]:= Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
+{ParentDirectory[NotebookDirectory[]],"data","topologies.txt"}],"TSV"]
 
 importAbundanceTopol::usage="importAbundanceTopol[  ] importa la lista de
 topologías y su abundancia -> {topología, abundancia}."
 
-importAbundanceTopol[]:= Flatten[ToExpression[#],1]&/@Import["/home/nesper94/Documentos/biologia-de-sistemas/RobustnessModularityProject/RobustnessModularityProject/motivos_de_red.txt","TSV"]
+importAbundanceTopol[]:= Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
+{ParentDirectory[NotebookDirectory[]],"data","topologies-with-abundances.txt"}],"TSV"]
 
 importProcrustedGRNs::usage="importProcrustedGRNs[  ]"
 
-importProcrustedGRNs[]:=Flatten[ToExpression[#],1]&/@Import[ FileNameJoin[
+importProcrustedGRNs[]:=Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
 {ParentDirectory[NotebookDirectory[]],"data","procrusted-grns.txt"}],"TSV"]
