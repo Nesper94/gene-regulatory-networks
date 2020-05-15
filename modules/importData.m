0d94f5d0a60d90de7a561cbf1ea580d8c4fa48a6
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2020-05-14 20:20:42 -0500

    load data both from notebook and script

--- modules/importData.m
+++ modules/importData.m
@@ -1,26 +1,30 @@
 (*Package to import data
 Author: Juan Camilo Arboleda Rivera
-Last modified: 2020-05-04*)
+Last modified: 2020-05-14*)
+
+(* Get module path *)
+path = $InputFileName /. "" :> NotebookFileName[];
+(* parentDir = DirectoryName @ parentPath; *)
 
 importGRNs::usage="importGRNs[  ] importa la lista de GRNs clasificadas no
 isomórficas -> {GRN, número de topología}."
 
 importGRNs[]:= Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
-{ParentDirectory[NotebookDirectory[]],"data","ordered-non-isomorph-grns.txt"}],"TSV"]
+{ParentDirectory[ DirectoryName @ path ],"data","ordered-non-isomorph-grns.txt"}],"TSV"]
 
 importTopologies::usage="importTopologies[  ] importa la lista de topologías
 ordenadas según su respectiva abundancia -> {topología, ranking de abundancia}."
 
 importTopologies[]:= Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
-{ParentDirectory[NotebookDirectory[]],"data","topologies.txt"}],"TSV"]
+{ParentDirectory[ DirectoryName @ path ],"data","topologies.txt"}],"TSV"]
 
 importAbundanceTopol::usage="importAbundanceTopol[  ] importa la lista de
 topologías y su abundancia -> {topología, abundancia}."
 
 importAbundanceTopol[]:= Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
-{ParentDirectory[NotebookDirectory[]],"data","topologies-with-abundances.txt"}],"TSV"]
+{ParentDirectory[ DirectoryName @ path ],"data","topologies-with-abundances.txt"}],"TSV"]
 
 importProcrustedGRNs::usage="importProcrustedGRNs[  ]"
 
 importProcrustedGRNs[]:=Flatten[ToExpression[#],1] &/@ Import[ FileNameJoin[
-{ParentDirectory[NotebookDirectory[]],"data","procrusted-grns.txt"}],"TSV"]
+{ParentDirectory[ DirectoryName @ path ],"data","procrusted-grns.txt"}],"TSV"]
