57557428215d2bf480407de30761b707a0dab0d3
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2021-05-26 16:34:49 -0500

    Reformat usage function

--- modules/calculate_h.m
+++ modules/calculate_h.m
@@ -1,8 +1,5 @@
-calculateMorphDecay::usage="calculateMorphDecay[NumNuclei_: 30, A0_: 1]
-calculates the value of the morphogen decay parameter (h) for morphogenetic
-fields with different numbers of nuclei so that the concentration in the last
-cell is equal to A0*Exp[-29/(30*0.4)]"
-calculateMorphDecay[NumNuclei_: 30, A0_: 1] := Block[
+calculateMorphDecay::usage="calculateMorphDecay[NumNuclei_:30, A0_:1] calculates the value of the morphogen decay parameter (h) for morphogenetic fields with different numbers of nuclei given by NumNuclei so that the concentration in the last cell is equal to A0*Exp[-29/(30*0.4)]"
+calculateMorphDecay[NumNuclei_:30, A0_:1] := Block[
     {rightEndConcentration, h},
 
   rightEndConcentration = A0*Exp[-29/(30*0.4)];
