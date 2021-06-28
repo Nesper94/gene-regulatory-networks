5169484514058ccabdd644bb66010d9d17e9fa29
Juan C. Arboleda R <juan.arboleda2@udea.edu.co>
2021-06-27 22:31:09 -0500

    Set delayed OptimalPattern and Dmax
    If NumNuclei is changed, then the value of OptimalPattern and
    Dmax should be changed too, if they are set normally, they
    wouldn't change its value in response to NumNuclei.

--- modules/parameters.m
+++ modules/parameters.m
@@ -19,13 +19,13 @@ IntTime = 500.;
 (*Optimal expression pattern represented in a scale between 1-10 in
 expression level, being 10 an expression level which is > 90% of the
 maximal level observed along the 1D field of cells for the output genes*)
-OptimalPattern =
+OptimalPattern :=
  Flatten[{ConstantArray[1, Quotient[NumNuclei, 3]],
    ConstantArray[10, Quotient[NumNuclei, 3] + Mod[NumNuclei, 3]],
    ConstantArray[1, Quotient[NumNuclei, 3]]}];
 
 (*Maximal discrepancy achievable for a given pattern w.r.t to the optimal pattern above*)
-Dmax=9*NumNuclei;
+Dmax := 9*NumNuclei;
 
 End[]
 EndPackage[]
